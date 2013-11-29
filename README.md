8ca32f4703ed495b5d72bff8f7fb54c141aded0c
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2013-11-29 20:47:45 +0100

    JAXRS 1.1 branch

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ restdoc-java-server
 ===================
 
 Java Server implementation of RestDoc
+
+JAX-RS 1.1 compatible version
--- src/test/java/org/restdoc/server/impl/MyResourceBean.java
+++ src/test/java/org/restdoc/server/impl/MyResourceBean.java
@@ -1,10 +1,11 @@
 package org.restdoc.server.impl;
 
-import javax.ws.rs.BeanParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 import org.restdoc.annotations.RestDoc;
+import org.restdoc.annotations.RestDocParam;
 
 @Path("/deep")
 public class MyResourceBean {
@@ -16,7 +17,7 @@ public class MyResourceBean {
 	
 	@GET
 	@RestDoc(resourceDescription = "blubb")
-	public void get(@BeanParam BeanObj o) {
-		System.out.println(o);
+	public void get(@QueryParam("color") @RestDocParam(description = "my special color") EColor color) {
+		System.out.println(color);
 	}
 }
